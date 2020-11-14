afc1398fab0a835fa099935c5cccc48ce117e889
Kesya Amanda <72657892+karacelly@users.noreply.github.com>
2020-11-15 02:25:07 +0700

    Update addDessert.cpp

--- addDessert.cpp
+++ addDessert.cpp
@@ -3,53 +3,53 @@
 #include <time.h>
 #include <stdlib.h>
 
-char typeMenu[100][255];
-char menuName[100][255];
-char topping[100][255];
-double calories[100];
-int currentMenu = 1;
-int menuPrice[100];
-int dessertTime[100];
+struct pesanDessert orderDessert[100];
 
 void addDesert();
 
-void addDesert(){
-    strcpy(typeMenu[currentMenu], "Dessert");
+void addDessert(){
+    strcpy(orderDessert[currentMenu].typeMenu, "Dessert");
     char namaMenu[255], toppingMenu[255];
     int price;
     double calorie;
 
     //input nama dessert
-    do
-    {
+    getchar();
+    do 
+	{
         printf ("Input the name [at least 5 characters]: ");
         scanf ("%[^\n]", namaMenu); getchar();
     }while(strlen(namaMenu) < 5);
-    strcpy(menuName[currentMenu], namaMenu);
+    strcpy(orderDessert[currentMenu].menuName, namaMenu);
 
     //input harga
-    do
-    {
+    do 
+	{
         printf ("Input the price [10 - 500]: "); 
         scanf ("%d", &price); getchar();
     }while(price < 10 || price > 500);
-    menuPrice[currentMenu] = price;
+    orderDessert[currentMenu].menuPrice = price;
 
     //input topping
-    do
-    {
+    int flag = 0;
+    do 
+	{
         printf ("Input the topping ['Caramel' | 'Honey' | 'Syrup'](Case Insensitive): ");
         scanf ("%s", toppingMenu); getchar();
-    }while(strcmp("Caramel", toppingMenu) != 0 || strcmp("Honey", toppingMenu) != 0 || strcmp("Syrup", toppingMenu) != 0);
-    strcpy(topping[currentMenu], toppingMenu);
+        if (toppingMenu[0] > 'Z') toppingMenu[0] -= 32;
+        if(strcmp("Caramel", toppingMenu) == 0 || strcmp("Honey", toppingMenu) == 0 || strcmp("Syrup", toppingMenu) == 0){
+            flag = 1;
+        }
+    }while(flag == 0);
+    strcpy(orderDessert[currentMenu].topping, toppingMenu);
 
     //input kalori
-    do
-    {
+    do 
+	{
         printf ("Insert calories [1.00 - 99.00]: ");
         scanf ("%lf", &calorie); getchar();
     }while(calorie < 1.00 || calorie > 99.00);
-    calories[currentMenu] = calorie;
+    orderDessert[currentMenu].calories = calorie;
     printf("Successfully added a new menu!");
     getchar();
     currentMenu++;
@@ -57,18 +57,15 @@ void addDesert(){
     //ngitung cooking time
     //random waktu pembuatan dessert(line 100)
     srand(time(0));
-    int totaltime = (rand() % 40)+50;
+    orderDessert[currentMenu].dessertTime = (rand() % 40)+50;
 
     if(strcmp(toppingMenu, "Caramel") == 0){
-        totaltime += 10;
+        orderDessert[currentMenu].dessertTime += 10;
     }
     else if(strcmp(toppingMenu, "Honey") == 0){
-        totaltime+=15;
+        orderDessert[currentMenu].dessertTime += 15;
     }
     else if(strcmp(toppingMenu, "Syrup") == 0){
-        totaltime+=20;
+        orderDessert[currentMenu].dessertTime +=20;
     }
-
-    dessertTime[currentMenu] = totaltime;
-    
 }
