71fe06f98de86347cb328b3f19fe6051d03b2685
cherryxx-oss <73350472+cherryxx-oss@users.noreply.github.com>
2020-11-16 14:40:28 +0700

    Add files via upload

--- addDessert.cpp
+++ addDessert.cpp
@@ -3,18 +3,23 @@
 #include <time.h>
 #include <stdlib.h>
 
-char typeMenu[100][255];
-char menuName[100][255];
-char topping[100][255];
-double calories[100];
+struct pesanDessert{
+    char typeMenu[255];
+    char menuName[255];
+    char topping[255];
+    double calories;
+    int currentMenu = 1;
+    int menuPrice;
+    int dessertTime;
+};
+
 int currentMenu = 1;
-int menuPrice[100];
-int dessertTime[100];
+pesanDessert orderDessert[105];
 
 void addDesert();
 
-void addDesert(){
-    strcpy(typeMenu[currentMenu], "Dessert");
+int main(){
+    strcpy(orderDessert[currentMenu].typeMenu, "Dessert");
     char namaMenu[255], toppingMenu[255];
     int price;
     double calorie;
@@ -25,14 +30,14 @@ void addDesert(){
         printf ("Input the name [at least 5 characters]: ");
         scanf ("%[^\n]", namaMenu); getchar();
     }while(strlen(namaMenu) < 5);
-    strcpy(menuName[currentMenu], namaMenu);
+    strcpy(orderDessert[currentMenu].menuName, namaMenu);
 
     //input harga
     do {
-        printf ("Input the price [10 - 500]: "); 
+        printf ("Input the price [10 - 500]: $ "); 
         scanf ("%d", &price); getchar();
     }while(price < 10 || price > 500);
-    menuPrice[currentMenu] = price;
+    orderDessert[currentMenu].menuPrice = price;
 
     //input topping
     int flag = 0;
@@ -44,20 +49,17 @@ void addDesert(){
             flag = 1;
         }
     }while(flag == 0);
-    strcpy(topping[currentMenu], toppingMenu);
+    strcpy(orderDessert[currentMenu].topping, toppingMenu);
 
     //input kalori
     do {
         printf ("Insert calories [1.00 - 99.00]: ");
         scanf ("%lf", &calorie); getchar();
     }while(calorie < 1.00 || calorie > 99.00);
-    calories[currentMenu] = calorie;
-    printf("Successfully added a new menu!");
-    getchar();
-    currentMenu++;
+    orderDessert[currentMenu].calories = calorie;
 
-    //ngitung cooking time
-    //random waktu pembuatan dessert(line 100)
+    //Mencari dessertTime
+    //random waktu pembuatan dessert
     srand(time(0));
     int totaltime = (rand() % 40)+50;
 
@@ -65,12 +67,17 @@ void addDesert(){
         totaltime += 10;
     }
     else if(strcmp(toppingMenu, "Honey") == 0){
-        totaltime+=15;
+        totaltime += 15;
     }
     else if(strcmp(toppingMenu, "Syrup") == 0){
-        totaltime+=20;
+        totaltime += 20;
     }
 
-    dessertTime[currentMenu] = totaltime;
-    
+    orderDessert[currentMenu].dessertTime = totaltime;
+
+    printf("\nSuccessfully added a new menu!");
+    getchar();
+    currentMenu++;
 }
+
+
